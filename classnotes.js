2cf5c3310f3db43dfecabd0ad00325fac30d49c2
dannevernguyens <danguyenuw@gmail.com>
2020-03-21 15:15:37 -0700

    adding more class notes

--- classnotes.js
+++ classnotes.js
@@ -166,3 +166,204 @@ let edgeInputValueC = 2;
 //DATABASE LECTURE NOTES
 ////////
 
+const noteSchema = {
+  id: {type: 'string', required: true},
+  note: {type: 'string', required: true},
+  categoryID: {type: 'string', required: false},
+}
+
+// Mongo DB notes
+
+//install mongo globally (done)
+// make new data folder
+// put package.json in there
+/// mongod --dbpath=[data/path] // ties your database to the thing as a local db, this will take awhile to run, leave it alone
+// access this db via mongo shell
+// a server is a thing that runs certain processes until told to stop
+// mongo [enter] should access your mongo shell/terminal, kinda like psql
+// mongo DB Atlas, client that helps us use mongodb
+
+// atlas calls dbs "clusters"
+// mongo DB compass - gui -
+// helpful tool for folks who dont want to use mongo via terminal cli
+
+// ATLAS jsut for windows users
+// need to use mongodb URL to connect to cluster
+// ex mongodb:localhost:PORTNUMBER // default is 27017
+// after connect, you should see "admin, config, local"
+// follow some steps
+// will output a terminal input that gives you access to prod database (not local) you linked in Atlas
+// ATLAS just for windows users
+
+////MONGOOSE NOTES////
+// do not commit data folder when submitting labs
+// files are too large for all that
+// will need to setup package.json file for data folder
+//will need to npm install mongoose within the directory of the project
+
+////////////////////////////////
+// index.js setup for mongoose usage
+//////////////////////
+
+const mongoose = require('mongoose');
+const dbURL = 'mongodb://localhost:27017/app';
+/// stuff after port num/ should be name of the db you are using
+const Notes = require('path to notes schema');
+const Categories = require('path to category schema');
+
+// START DB LINE
+//lets us connect to mongoose stuff, first param is dbURL, second param is object consisting of options, get the options from looking at stuff in mongoose install notes
+mongoose.connect(dbURL, {
+  useNewURLParser: true, useUnifiedTopology: true
+});
+// the above establishes connection to external database
+// so you wil have to specify when you want to disconnect from it
+
+// do some actions while in DB
+// these actions are async, will complete before disconnect happens
+
+let args = process.argv.slice(2);
+
+// here we will try to list all notes?
+
+const dbOperations = async () => {
+
+  // to add note to db
+  if (args.length > 0) {
+    // prob wanna save new note
+    //need to create new instance of model before we save
+    // model = notes
+    // build a piece of data that fits rules of the model
+    let newNote = new Notes({
+      //id auto gen
+      note: args[0],
+      //category doesnt matter here
+    });
+    try {
+      // use try/catch here because otherwise, error makes app hang
+      await newNote.save();
+      // saves full version of this note obj
+     } catch(e){
+       console.error(e);
+     }
+  }
+  
+  // to see all notes and categories
+  let allNotes = await Notes.find();
+  let allCats = await Categories.find();
+
+  //to find specific part of this stuff
+  // finds first cat w name of important
+  let importCat = await Categories.findOne({name: 'important'});
+  // find by id
+  let importCatId = importCat._id;
+
+  //filter to only notes that have category 'important'
+    let filterNotes = allNotes.filter(val => {
+      if(val.category && val.category.length)
+      // if it has a cat and cat has value
+      return val.category.includes(importCatId);
+      // return it if it matches that ID i care about
+    });
+
+    //to show that search/filter
+    filterNotes.forEach(val => {
+      console.log(val.note);
+    })
+
+  // to format output cleanly without extra JSON stuff 
+  allNotes.forEach( val => {
+    console.log(val.Notes);
+  });
+
+  console.log('---------');
+
+  allCats.forEach( val => {
+    console.log(val.Name);
+  })
+
+
+  //close the db after await
+  mongoose.disconnect();
+};
+
+dbOperations();
+
+// CLOSE DB LINE at the end
+// mongoose.disconnect();
+
+// to handle mongoose schema, create new folder outside of data folder called "models". this is for data modeling. then new file called "notes-schema.js"
+
+// try to add new note
+
+
+
+/////////////////////////////
+// notes-schema.js
+////////////
+
+'use strict';
+
+const mongoose = require('mongoose');
+// mongoose is our package
+/// mongoose exports a class Schema
+// wea re passng our schema obj as a param to the Schema constructor
+// this should return an instance of mongoose.Schema
+
+const notesSchema = {
+    id: { type: 'string', required: true},
+    note: { type: 'string', required: true},
+    category: { type: 'array'}
+};
+// generic schema above
+// mongoose schema below, passing in obj as param
+
+const notesSchema = mongoose.Schema({
+  id: { type: 'string', required: true},
+  note: { type: 'string', required: true},
+  category: { type: 'array'}
+});
+
+// in mongoose, defined in that package, simple ex below
+class Schema {
+  constructor(obj){
+    /// does stuff w obj
+  }
+}
+
+let mySchema = new Schema({blah})
+
+// mongoose diff syntax schema types are on the mongoose documentation site. BELOW IS CURRENT SYNTAX
+const notesSchema = mongoose.Schema({
+  // _id: { type: mongoose.Types.ObjectId, required: true},
+  // don tneed to add id, it is automatic i think
+  note: { type: String, required: true},
+  category: { type: Array}
+});
+
+// want to build data model from this schema
+// first param, name of collection (plural), mongo always adds an S at end
+// second param, X Schema
+const notesModel = mongoose.model('notes', notesSchema);
+// now we can interact with the notesModel rather than the schema
+// all this to create a model for our notes
+// this lets mongoose do lots of validation for us
+
+module.exports = notesModel;
+
+/////
+////////////////////
+///// categories schema build
+/// categories-schema.js
+//////////////
+
+'use strict';
+const mongoose = require('mongoose');
+
+const categoriesSchema = mongoose.Schema({
+  name: { type: String, required: true}
+});
+
+const categoriesModel = mongoose.model('categories', categoriesSchema);
+
+module.exports = categoriesModel;
